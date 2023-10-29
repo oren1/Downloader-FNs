eec7b713dad2ef5386a4c9e70b335c6ac7aabdaf
Oren Shalev <orenico4@gmail.com>
2023-10-29 09:25:08 +0200

    first commit

---
+++ functions/index.js
@@ -0,0 +1,73 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const ytdl = require('ytdl-core')
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
+
+
+exports.videoInfo = onRequest(async (req, res) => {
+    // Grab the urk parameter.
+     const url = decodeURIComponent(req.body.url);
+     logger.log("url", url);
+     let info = await ytdl.getInfo(url)
+     logger.log("info", info);
+     let videoInfo = info.player_response.streamingData.formats[0]
+    //  let narrowedInfo = ytdl.chooseFormat(info.formats, {quality: 'highest'});
+     res.json({result: videoInfo});
+});
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addmessage = onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await getFirestore()
+        .collection("messages")
+        .add({original: original});
+    // Send back a message that we've successfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+
+
+  // Listens for new messages added to /messages/:documentId/original
+// and saves an uppercased version of the message
+// to /messages/:documentId/uppercase
+exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
+    // Grab the current value of what was written to Firestore.
+    const original = event.data.data().original;
+  
+    // Access the parameter `{documentId}` with `event.params`
+    logger.log("Uppercasing", event.params.documentId, original);
+  
+    const uppercase = original.toUpperCase();
+  
+    // You must return a Promise when performing
+    // asynchronous tasks inside a function
+    // such as writing to Firestore.
+    // Setting an 'uppercase' field in Firestore document returns a Promise.
+    return event.data.ref.set({uppercase}, {merge: true});
+  });
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
